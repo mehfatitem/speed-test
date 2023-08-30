ca9b7fa848a1a60dddbaaec685c84f45f0e90a87
Mehmed Fatih Temiz <temizfatih54@gmail.com>
2023-08-31 02:06:00 +0300

    refactor the script.js make shorter

--- client/js/script.js
+++ client/js/script.js
@@ -1,27 +1,9 @@
 angular.module('SpeedTestApp', [])
   .controller('SpeedTestController', function($scope, $http) {
 
-    $scope.addDarkness = function() {
-      angular.element(document.body).addClass('make_darkness');
-    };
+    const toggleDarkness = () => angular.element(document.body).toggleClass('make_darkness');
 
-    $scope.removeDarkness = function() {
-      angular.element(document.body).removeClass('make_darkness');
-    };
-
-    $scope.ping = 'N/A';
-    $scope.download = 'N/A';
-    $scope.upload = 'N/A';
-    $scope.country = '';
-    $scope.name = '';
-    $scope.location = '';
-    $scope.host = '';
-    $scope.ip = '';
-    $scope.clientIp = ''; // Initialize clientIp
-    $scope.showResults = true;
-    $scope.isTesting = false;
-
-    $scope.startSpeedTest = function() {
+    $scope.resetData = () => {
       $scope.ping = 'N/A';
       $scope.download = 'N/A';
       $scope.upload = 'N/A';
@@ -30,31 +12,38 @@ angular.module('SpeedTestApp', [])
       $scope.location = '';
       $scope.host = '';
       $scope.ip = '';
-      $scope.clientIp = ''; // Reset clientIp
-      $scope.addDarkness();
+      $scope.clientIp = '';
+      $scope.showResults = false;
+      $scope.isTesting = false;
+    };
+
+    $scope.startSpeedTest = () => {
       if (!$scope.isTesting) {
+        toggleDarkness();
+        $scope.resetData();
         $scope.isTesting = true;
-        $scope.showResults = false;
         $http.get('/speedtest')
-          .then(function(response) {
+          .then(response => {
             const data = response.data;
-            $scope.ping = data.ping;
-            $scope.download = data.download;
-            $scope.upload = data.upload;
-            $scope.name = data.server.name;
-            $scope.ip = data.server.ip;
-            $scope.country = data.server.country;
-            $scope.host = data.server.host;
-            $scope.location = data.server.location;
-            $scope.clientIp = data.clientIp; // Set clientIp
-            $scope.showResults = true;
+            Object.assign($scope, {
+              ping: data.ping,
+              download: data.download,
+              upload: data.upload,
+              name: data.server.name,
+              ip: data.server.ip,
+              country: data.server.country,
+              host: data.server.host,
+              location: data.server.location,
+              clientIp: data.clientIp,
+              showResults: true,
+            });
           })
-          .catch(function(error) {
+          .catch(error => {
             console.error('Error fetching speed test results:', error);
           })
-          .finally(function() {
+          .finally(() => {
             $scope.isTesting = false;
-            $scope.removeDarkness();
+            toggleDarkness();
           });
       }
     };
